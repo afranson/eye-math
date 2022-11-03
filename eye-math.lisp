088675df23497469bd7866c94f41da6ecf25a62c
Andrew Franson <fransonspace@msn.com>
2022-11-03 00:47:30 -0400

    Create defeyefun macro to update function-arg-format list automatically, no more double entry.

--- eye-math.lisp
+++ eye-math.lisp
@@ -65,96 +65,128 @@ xkey function extract the x value from the structure, ykey the y value."
 ;; Put around function (like python decorator, but better) - e.g. (with-num-protection (defun ...))
 ;; Searches function body for occurance of any element in var-names and replaces it with
 ;; (check-for-num element) so if the argument is not a number, function fails gracefully
-(defmacro with-num-protection ((&rest var-names) &body body)
-  (labels ((map-tree (f tree)
-	     (when tree
-	       (if (atom tree)
-		   (funcall f tree)
-		   (if (eq (car tree) 'defun) ;; if on the defun line, skip straight to body
-		       (apply #'list (car tree) (cadr tree) (caddr tree) (map-tree f (cdddr tree)))
-		       (if (nthcdr 1 tree)
-			   (apply #'list (map-tree f (car tree)) (map-tree f (cdr tree)))
-			   (list (map-tree f (car tree)))))))))
-    `,(map-tree #'(lambda (x) (if (member x var-names) `(check-for-num ,x) x)) (elt body 0))))
+;; (defmacro with-num-protection ((&rest var-names) &body body)
+;;   (labels ((map-tree (f tree)
+;; 	     (when tree
+;; 	       (if (atom tree)
+;; 		   (funcall f tree)
+;; 		   (if (eq (car tree) 'defun) ;; if on the defun line, skip straight to body
+;; 		       (apply #'list (car tree) (cadr tree) (caddr tree) (map-tree f (cdddr tree)))
+;; 		       (if (nthcdr 1 tree)
+;; 			   (apply #'list (map-tree f (car tree)) (map-tree f (cdr tree)))
+;; 			   (list (map-tree f (car tree)))))))))
+;;     `,(map-tree #'(lambda (x) (if (member x var-names) `(check-for-num ,x) x)) (elt body 0))))
 
 
 
-(with-num-protection (distance)
-  (defun first-blur (distance &optional (unit :in) &rest trash)
-    "Return the diopter value if distance is the first time blur is observed"
-    (declare (ignore trash))
-    (/ 1 (get-conversion unit) distance)))
+(defparameter *full-methods* nil "Lookup table for function, parameters, and formatting output")
 
-(with-num-protection (read-distance read-size-20/x chart-distance lenses)
- (defun acuity->diopters (read-distance &optional (read-size-20/x 20) (chart-distance 14) (lenses 0) (unit :in) &rest trash)
-   "Return the diopter value if read-distance is the first point that the text at read-size-20/x (20/20 or 20/30 for example) is readable"
-   (declare (ignore trash))
-   (let ((effective-acuity (* read-size-20/x chart-distance (/ read-distance))))
-     (when (not (<= 15 effective-acuity 50))
-       (format t "Warning: Unreliable acuity extrapolation, 20/~D~%" (floor effective-acuity)))
-     (+ (abs lenses)
-	(- (interpolate-from-list *acuity->diopter-list* effective-acuity))
-	(first-blur read-distance unit)))))
-
-(with-num-protection (full-prescription lenses)
- (defun proper-distance (full-prescription &optional (lenses 0) (unit :m) &rest trash)
-   "Returns the max viewing distance for clear vision given a full-prescription and current worn lenses"
-   (declare (ignore trash))
-   (list (/ 1 (- full-prescription lenses) (get-conversion unit)) unit)))
+(defmacro defeyefun (name lambda-list must-be-nums format-style &body body)
+  (labels ((check-for-doc-and-declare (body)
+	     (cond ((and (stringp (car body)) (eq (caadr body) 'declare)) 2)
+		   ((and (stringp (car body)) (not (eq (caadr body) 'declare))) 1)
+		   ((eq (caar body) 'declare) 1)
+		   (t 0)))
+	   (lambda-to-arg-names (lambda-item)
+	     (if (member lambda-item '(&optional &key &rest trash))
+		 nil
+		 (list (sb-unicode:lowercase (if (consp lambda-item)
+						 (symbol-name (car lambda-item))
+						 (symbol-name lambda-item)))))))
+    `(progn
+       (defun ,name ,lambda-list
+	 ,@(append (subseq body 0 (check-for-doc-and-declare body))
+		   (list `(let ,(mapcar #'(lambda (y) `(,y (check-for-num ,y))) must-be-nums)
+			    ,@(subseq body (check-for-doc-and-declare body))))))
+       (push (list #',name (list ,(sb-unicode:lowercase (symbol-name name)) ,@(mapcan #'lambda-to-arg-names lambda-list)) ,format-style) *full-methods*))))
 
 
-(with-num-protection (full-prescription distance)
- (defun proper-lens (full-prescription &optional (distance 1) (unit :m) &rest trash)
-   "Returns the proper diopter value lens that should be worn to clearly see something distance away"
-   (declare (ignore trash))
-   (- full-prescription (/ 1 (get-conversion unit) distance))))
+(defeyefun first-blur (distance &optional (unit :in) &rest trash)
+    (distance)
+    (lambda (x) (format t "Full Prescription: ~a Diopters~%" x))
+  "Return the diopter value if distance is the first time blur is observed"
+  (declare (ignore trash))
+  (/ 1 (get-conversion unit) distance))
+
+(defeyefun acuity->diopters (read-distance &optional (read-size-20/x 20) (chart-distance 14) (lenses 0) (unit :in) &rest trash)
+    (read-distance read-size-20/x chart-distance lenses)
+    (lambda (x) (format t "Full Prescription: ~a Diopters~%" x))
+  "Return the diopter value if read-distance is the first point that the text at read-size-20/x (20/20 or 20/30 for example) is readable"
+  (declare (ignore trash))
+  (let ((effective-acuity (* read-size-20/x chart-distance (/ read-distance))))
+    (when (not (<= 15 effective-acuity 50))
+      (format t "Warning: Unreliable acuity extrapolation, 20/~D~%" (floor effective-acuity)))
+    (+ (abs lenses)
+       (- (interpolate-from-list *acuity->diopter-list* effective-acuity))
+       (first-blur read-distance unit))))
+
+(defeyefun proper-distance (full-prescription &optional (lenses 0) (unit :m) &rest trash)
+    (full-prescription lenses)
+    (lambda (x y) (format t "Proper Viewing Distance: ~a ~a~%" x y))
+  "Returns the max viewing distance for clear vision given a full-prescription and current worn lenses"
+  (declare (ignore trash))
+  (list (/ 1 (- full-prescription lenses) (get-conversion unit)) unit))
+
+(defeyefun proper-lens (full-prescription &optional (distance 1) (unit :m) &rest trash)
+    (full-prescription distance)
+    (lambda (x) (format t "Proper Lenses: ~a Diopters~%" x))
+  "Returns the proper diopter value lens that should be worn to clearly see something distance away"
+  (declare (ignore trash))
+  (- full-prescription (/ 1 (get-conversion unit) distance)))
+
+(defeyefun correction-delta (full-prescription &optional (lenses 0) (distance 1) (unit :m) &rest trash)
+    (full-prescription lenses distance)
+    (lambda (x) (format t "Correction Delta: ~a Diopters~%" x))
+  "Give the diopter gap between the appropriate perscription to view something distance away with full-prescription vs the lenses currently worn"
+  (declare (ignore trash))
+  (- (proper-lens full-prescription distance unit) lenses))
 
 
-(with-num-protection (full-prescription lenses distance)
-  (defun correction-delta (full-prescription &optional (lenses 0) (distance 1) (unit :m) &rest trash)
-    "Give the diopter gap between the appropriate perscription to view something distance away with full-prescription vs the lenses currently worn"
-    (declare (ignore trash))
-    (- (proper-lens full-prescription distance unit) lenses)))
+(defeyefun get-all (total-first-to-blur-distance &optional (20/20-acuity-distance 9.75))
+    (total-first-to-blur-distance 20/20-acuity-distance)
+    (lambda (x y z a) (format t "Total: ~4,2f  Center: ~4,2f  Spherical: ~4,2f  Astigmatism: ~4,2f Diopters~%" x y z a))
+  "Returns the astigmatism, total correction (astig + sph), and spherical correction in diopters given a total-first-to-blur-distance along the hard axis (the astigmatism direction, and a 20/20-acuity-distance (the furthest distance 20/20 is readable from on a 14 in Snellen chart with no correction"
+  (let ((sph (acuity->diopters 20/20-acuity-distance))
+	(tot (first-blur total-first-to-blur-distance)))
+    (list tot (/ (+ tot sph) 2) sph (- tot sph)))) ;; total, center, spherical, astig
+
+(defeyefun convert-units (distance &optional (unit-from :m) (unit-to :in) &rest trash)
+    (distance)
+    (lambda (x y) (format t "Converted Distance: ~a ~a~%" x y))
+  "Converts between units using the conversions hash table. Does meters, inches, feet, and centimeters."
+  (declare (ignore trash))
+  (list (/ (* distance (get-conversion unit-from)) (get-conversion unit-to)) unit-to))
+
+(defeyefun diopters->acuity (full-prescription &optional (distance 14) (unit :in) (lenses 0))
+    (full-prescription distance lenses)
+    (lambda (x) (format t "Reading Acuity: 20/~a~%" (round x)))
+  "Converts a reading environment into an expected acuity value"
+  (interpolate-from-list *acuity->diopter-list* (- (correction-delta full-prescription lenses distance unit)) :pos-keys (list #'second #'first)))
+
+(defeyefun nearest-safe-dist (lenses &optional (full-prescription 4.75) (unit :cm) (optical-range 6))
+    (lenses full-prescription optical-range)
+    (list (lambda (x y) (format t "Minimum usable distance: ~a ~a~%" x y)))
+  "Returns the minimum viable viewing distance given a prescription and lens strength. Anything closer than this will guarantee a myopic stimulus."
+  (list (/ 1 (+ (- lenses) full-prescription optical-range) (get-conversion unit))
+	unit))
+
+(defun make-methods (funcs)
+  (mapcar #'(lambda (x)
+	      (find x *full-methods* :test #'(lambda (y z) (eq y (car z)))))
+	  funcs))
+
+(defparameter methods (make-methods (list #'acuity->diopters
+					  #'diopters->acuity
+					  #'first-blur
+					  #'get-all
+					  #'proper-distance
+					  #'nearest-safe-dist
+					  #'proper-lens
+					  #'correction-delta
+					  #'convert-units)))
 
 
-(with-num-protection (total-first-to-blur-distance 20/20-acuity-distance)
- (defun get-all (total-first-to-blur-distance &optional (20/20-acuity-distance 9.75))
-   "Returns the astigmatism, total correction (astig + sph), and spherical correction in diopters given a total-first-to-blur-distance along the hard axis (the astigmatism direction, and a 20/20-acuity-distance (the furthest distance 20/20 is readable from on a 14 in Snellen chart with no correction"
-   (let ((sph (acuity->diopters 20/20-acuity-distance))
-	 (tot (first-blur total-first-to-blur-distance)))
-     (list tot (/ (+ tot sph) 2) sph (- tot sph))))) ;; total, center, spherical, astig
-
-(with-num-protection (distance)
- (defun convert-units (distance &optional (unit-from :m) (unit-to :in) &rest trash)
-   "Converts between units using the conversions hash table. Does meters, inches, feet, and centimeters."
-   (declare (ignore trash))
-   (list (/ (* distance (get-conversion unit-from)) (get-conversion unit-to)) unit-to)))
-
-(with-num-protection (full-prescription distance lenses)
-  (defun diopters->acuity (full-prescription &optional (distance 14) (unit :in) (lenses 0))
-    "Converts a reading environment into an expected acuity value"
-    (interpolate-from-list *acuity->diopter-list* (- (correction-delta full-prescription lenses distance unit)) :pos-keys (list #'second #'first))))
-
-(with-num-protection (lenses full-prescription optical-range)
-  (defun nearest-safe-dist (lenses &optional (full-prescription 4.75) (unit :cm) (optical-range 6))
-    (list (/ 1 (+ (- lenses) full-prescription optical-range) (get-conversion unit))
-	  unit)))
-
-
-;; TODO would be great to just give function name (first field) and have all named parameters looked up automatically and added to the second field
 ;;; CLI Strings, Utilities, and Main function
-(defparameter methods
-  `(( (,#'acuity->diopters)  ("acuity->diopters" "read-dist" "read-size-20/x" "chart-dist" "lenses" "unit" ) (,(lambda (x) (format t "Full Prescription: ~a Diopters~%" x))))
-    ( (,#'diopters->acuity)  ("diopters->acuity" "full-prescription" "distance" "unit" "lenses" )            (,(lambda (x) (format t "Reading Acuity: 20/~a~%" (round x)))))
-    ( (,#'first-blur)        ("first-blur" "distance" "unit")                                                (,(lambda (x) (format t "Full Prescription: ~a Diopters~%" x))))
-    ( (,#'get-all) ("get-all" "first-blur-ast" "acuity-read-dist")                       (,(lambda (x y z a) (format t "Total: ~4,2f  Center: ~4,2f  Spherical: ~4,2f  Astigmatism: ~4,2f Diopters~%" x y z a))))
-    ( (,#'proper-distance)   ("proper-distance" "full-prescr" "lenses" "unit" )                              (,(lambda (x y) (format t "Proper Viewing Distance: ~a ~a~%" x y))))
-    ( (,#'nearest-safe-dist) ("nearest-safe-dist" "lenses" "full-prescription" "unit" "optical-range")       (,(lambda (x y) (format t "Minimum usable distance: ~a ~a~%" x y))))
-    ( (,#'proper-lens)       ("proper-lens" "full-prescr" "dist" "unit" )                                    (,(lambda (x) (format t "Proper Lenses: ~a Diopters~%" x))))
-    ( (,#'correction-delta)  ("correction-delta" "full-prescr" "lenses" "dist" "unit" )                      (,(lambda (x) (format t "Correction Delta: ~a Diopters~%" x))))
-    ( (,#'convert-units)     ("convert-units" "distance" "unit-from" "unit-to")                              (,(lambda (x y) (format t "Converted Distance: ~a ~a~%" x y)))))
-  "List that couples the functions to their parameters to their printing format")
-
 (defparameter function-info (make-hash-table) "Hash table relating function names to their information and formats")
 (mapcar #'(lambda (x) (setf (gethash x function-info) (elt methods (- x 1)))) (loop for x from 1 to (length methods) collect x))
 
@@ -177,11 +209,11 @@ xkey function extract the x value from the structure, ykey the y value."
 
 (defun get-func-from-hash (inputs)
   "Extract the function from the function-info hash table (specified by 0th element of inputs)"
-  (elt (elt (gethash (elt inputs 0) function-info) 0) 0))
+  (elt (gethash (elt inputs 0) function-info) 0))
 
 (defun get-format-from-hash (inputs)
   "Extract the format for printing the result of the function in the function-info hash table (specified by 0th element of inputs)"
-  (elt (elt (gethash (elt inputs 0) function-info) 2) 0))
+  (elt (gethash (elt inputs 0) function-info) 2))
 
 (defun apply-hash-func-to-inputs (inputs)
   "Get result of the function in hash table applied to the inputs (1st to last)"
